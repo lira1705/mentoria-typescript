ba35892086bcfa5430a190f03fdcd167891df5a1
Joao Pedro Lira Dos Santos <jpls@cin.ufpe.br>
2021-07-14 08:04:00 -0300

    Uso de variável do tipo string

--- app.ts
+++ app.ts
@@ -3,21 +3,22 @@ const input1 = document.getElementById('input1') as HTMLInputElement;
 const input2 = document.getElementById('input2') as HTMLInputElement;
 
 /**
- * Agora inserimos o parâmetro booleano printarResultado, que vai ajudar a definir se também vamos printar o resultado.
+ * Agora inserimos o parâmetro booleano frase, para deixar o print mais personalizado.
  */
-function somaValidaComPrint(numero1: number, numero2: number, printarResultado: boolean) { 
+function somaValidaComPrint(numero1: number, numero2: number, printarResultado: boolean, frase: string) { 
+    const resultado = numero1 + numero2;
     if (printarResultado) {
-        console.log(numero1 + numero2)
+        console.log(frase + resultado);
     }
-    return numero1 + numero2;
+    return resultado;
 }
 
 let printarResultado = true;
+let frase = "O número é ";
 
 // Como a busca pelo button na linha 1 pode trazer um null (já que esse id pode não estar presente no index.html), é importante confirmar se ele tem um valor
 if (button) {
     button.addEventListener('click', () => {
-        // Estamos passando a variável printarResultado como parâmetro para a função. Como ela contém um valor booleano, o typescript aceita normalmente.
-        console.log(somaValidaComPrint(Number(input1.value), Number(input2.value), printarResultado));
+        console.log(somaValidaComPrint(Number(input1.value), Number(input2.value), printarResultado, frase));
     }); 
 }
