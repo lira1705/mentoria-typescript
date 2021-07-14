dccad3db7c3b17bc6e5593f42fdeaa46f71c5aec
Joao Pedro Lira Dos Santos <jpls@cin.ufpe.br>
2021-07-14 08:15:33 -0300

    Repassando código do app.ts para o basico.ts com comentários atualizados

--- exemplos/basico.ts
+++ exemplos/basico.ts
@@ -2,6 +2,9 @@ const button = document.getElementById('button');
 const input1 = document.getElementById('input1') as HTMLInputElement;
 const input2 = document.getElementById('input2') as HTMLInputElement;
 
+/**
+ * Exemplo de função com parâmetros tipados
+ */
 function somaValidaComPrint(numero1: number, numero2: number, printarResultado: boolean, frase: string) { 
     const resultado = numero1 + numero2;
     if (printarResultado) {
@@ -10,12 +13,11 @@ function somaValidaComPrint(numero1: number, numero2: number, printarResultado: 
     return resultado;
 }
 
-// Vocês já devem ter percebido que o typescript infere o tipo da variável de acordo com o valor com a qual ela é inicializada
-// Como no caso abaixo em que ele já sabe que printarResultado é um booleano
+// A variável abaixo tem seu tipo inferido pelo valor inicial
 let printarResultado = true;
-// Mas também é possível escrever o tipo da variável de forma explícita
+// já nesse caso estamos dizendo explicitamente qual é o seu tipo
 let frase: string;
-// E só depois iniciar ela
+// E só depois iniciamos ela com um valor
 frase = "O número é ";
 
 // Como a busca pelo button na linha 1 pode trazer um null (já que esse id pode não estar presente no index.html), é importante confirmar se ele tem um valor
