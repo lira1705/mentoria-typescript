eb0310c65ca8d3e8c5715331e32f4cc48d11534c
Joao Pedro Lira Dos Santos <jpls@cin.ufpe.br>
2021-07-14 07:58:42 -0300

    Uso de variável do tipo boolean

--- app.ts
+++ app.ts
@@ -3,17 +3,21 @@ const input1 = document.getElementById('input1') as HTMLInputElement;
 const input2 = document.getElementById('input2') as HTMLInputElement;
 
 /**
- * Diferente do exemplo de JS anterior, essa função valida se seus parâmetros são número
- * Se somaValida("4", 5) for escrito, o typescript vai levantar um erro e dizer que o tipo string não é um number
+ * Agora inserimos o parâmetro booleano printarResultado, que vai ajudar a definir se também vamos printar o resultado.
  */
-function somaValida(numero1: number, numero2: number) { 
+function somaValidaComPrint(numero1: number, numero2: number, printarResultado: boolean) { 
+    if (printarResultado) {
+        console.log(numero1 + numero2)
+    }
     return numero1 + numero2;
 }
+
+let printarResultado = true;
 
 // Como a busca pelo button na linha 1 pode trazer um null (já que esse id pode não estar presente no index.html), é importante confirmar se ele tem um valor
 if (button) {
     button.addEventListener('click', () => {
-        // Como a função somaValida espera dois numbers, aqui fazemos a conversão
-        console.log(somaValida(Number(input1.value), Number(input2.value)));
+        // Estamos passando a variável printarResultado como parâmetro para a função. Como ela contém um valor booleano, o typescript aceita normalmente.
+        console.log(somaValidaComPrint(Number(input1.value), Number(input2.value), printarResultado));
     }); 
 }
