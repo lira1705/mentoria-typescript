bdebed97c4f1f458af0af513bac61f80a67a6ba6
Joao Pedro Lira Dos Santos <jpls@cin.ufpe.br>
2021-07-14 09:00:14 -0300

    Criando tipos que combinam tipos

--- exemplos/combinando.ts
+++ exemplos/combinando.ts
@@ -1,9 +1,13 @@
 /**
- * Quando tipamos podemos passar mais de um tipo, como no caso abaixo em que cada parâmetro contém os tipos number e string
+ * Quando tipamos podemos passar mais de um tipo, como no caso abaixo em que cada parâmetro contém o tipo input, que é igual aos tipos number ou string
  * No exemplo abaixo também ocorre um tratamento interno na função para que ela lide com os diferentes tipos de cada parâmetro
 */
-function juntar(input1: number | string, input2: number | string) {
-    let resultado: number | string;
+
+// Também é possível criar tipos! E combinar tipos com esses tipos!
+type input = number | string;
+
+function juntar(input1: input, input2: input) {
+    let resultado: input;
     if (typeof input1 === 'string' || typeof input2 === 'string') {
         resultado = input1.toString() + input2.toString();
     } else {
